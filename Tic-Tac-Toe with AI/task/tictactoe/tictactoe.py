e1dde9435791c0181bb8099ed1801e4d7ca724b1
skyheatfet <70936076+skyheatfet@users.noreply.github.com>
2020-09-12 21:50:11 -0700

    9/12/20 populate

--- Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
@@ -6,25 +6,37 @@ class TicTacToe:
         self.keyboard_input = ''
         self.ttt_board = [[f'not_used_{x}:{y}' if x == 0 or y == 0 else None for y in range(4)] for x in range(4)]
 
-    def get_action(self, prompt):
+    @staticmethod
+    def get_action(prompt):
         print(prompt, end='')
         keyboard_input = str.upper(input())
         return keyboard_input
 
     def coordinate_validation(self):
-        if self.keyboard_input in self.valid_cells:
+        if (len(self.keyboard_input)) == 9:
+            for character in self.keyboard_input:
+                if character in self.valid_cells:
+                    continue
+                else:
+                    return False
             return True
-        else:
-            return False
+        return False
 
     def populate_coordinates(self):
-        print(self.keyboard_input)
+        for character_position in range(len(self.keyboard_input)):
+            for y in range(3, 0, -1):
+                for x in range(1, 4):
+                    self.ttt_board[x][y] = self.keyboard_input[character_position]
+        print(self.ttt_board)
 
     def get_coordinates(self):
         self.keyboard_input = 'None'
         while not self.coordinate_validation():
             self.keyboard_input = self.get_action('Enter cells: >')
-            self.populate_coordinates()
+        self.populate_coordinates()
+
+    def display_board(self):
+        pass
 
     def main(self):
         self.get_coordinates()
