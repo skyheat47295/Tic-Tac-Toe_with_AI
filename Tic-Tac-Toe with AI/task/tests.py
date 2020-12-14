c271533e8b8b9a606f304f1c2858f1cf7f77466d
skyheatfet <70936076+skyheatfet@users.noreply.github.com>
2020-12-13 19:42:52 -0700

    12.13.20

--- Tic-Tac-Toe with AI/task/tests.py
+++ Tic-Tac-Toe with AI/task/tests.py
@@ -1,8 +1,9 @@
+import random
+from copy import deepcopy
+from enum import Enum
+
 from hstest.stage_test import *
 from hstest.test_case import TestCase
-from enum import Enum
-from typing import List, Optional
-from copy import deepcopy
 
 CheckResult.correct = lambda: CheckResult(True, '')
 CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
@@ -25,6 +26,17 @@ def get_state(symbol):
         return None
 
 
+def state2char(state):
+    if state == FieldState.X:
+        return "X"
+    if state == FieldState.O:
+        return "O"
+    if state == FieldState.FREE:
+        return "_"
+    else:
+        return None
+
+
 class TicTacToeField:
 
     def __init__(self, *, field: str = '', constructed=None):
@@ -39,7 +51,7 @@ class TicTacToeField:
 
             for row in range(3):
                 for col in range(3):
-                    index = (2 - row) * 3 + col
+                    index = row * 3 + col
                     self.field[row][col] = get_state(field[index])
 
     def equal_to(self, other) -> bool:
@@ -72,9 +84,9 @@ class TicTacToeField:
 
     def is_close_to(self, other) -> bool:
         return (
-            self.equal_to(other)
-            or self.has_next_as(other)
-            or other.has_next_as(self)
+                self.equal_to(other)
+                or self.has_next_as(other)
+                or other.has_next_as(self)
         )
 
     @staticmethod
@@ -98,7 +110,7 @@ class TicTacToeField:
             [None for _ in range(3)] for _ in range(3)
         ]
 
-        y: int = 2
+        y: int = 0
 
         for line in lines:
             cols = line[2], line[4], line[6]
@@ -109,7 +121,7 @@ class TicTacToeField:
                     return None
                 field[y][x] = state
                 x += 1
-            y -= 1
+            y += 1
 
         return TicTacToeField(constructed=field)
 
@@ -164,78 +176,133 @@ def iterate_cells(initial: str) -> str:
 
 
 class TicTacToeTest(StageTest):
+    win = 0
+    turn = 0
+    manual_test_turns = [None, [0, 0], [1, 2], [1, 0], [2, 2], [2, 0]]
+
     def generate(self) -> List[TestCase]:
-        tests: List[TestCase] = []
+        return [TestCase(stdin=(["start easy easy", "2 2"] + [self.test for _ in range(50)] + ["exit"]),
+                         check_function=self.check_1) for _ in range(50)] + \
+               [TestCase(stdin="exit", check_function=self.final_check_easy)] + \
+               [TestCase(stdin="start easy easy\nexit", check_function=self.auto_test_check),
+                TestCase(stdin=["start user user", self.manual_test_1, self.manual_test_1, self.manual_test_1,
+                                self.manual_test_1, self.manual_test_1, self.manual_test_1_check]),
+                TestCase(stdin=["start user user", "1 1", self.manual_test_2_1, self.manual_test_2_2,
+                                self.manual_test_2_3])] + \
+               [TestCase(stdin=["start medium medium", "2 2"] + [self.test for _ in range(50)] + ["exit"],
+                         check_function=self.check_1) for _ in range(50)] + \
+               [TestCase(stdin="exit", check_function=self.final_check_medium)]
 
-        i: int = 0
-        for input in inputs:
-            full_move_input = iterate_cells(input)
+    # checking overlapping ###################################################
+    def manual_test_2_1(self, output):
+        tic_tac_toe_field: TicTacToeField = TicTacToeField.parse(output)
+        if tic_tac_toe_field is None:
+            return CheckResult.wrong("The game field is incorrect")
+        if not str(tic_tac_toe_field.field[0][0]).lower() == "fieldstate.x":
+            return CheckResult.wrong("The X was placed to a wrong position." +
+                                     "The X symbol was not found (" +
+                                     state2char(tic_tac_toe_field.field[0][0]) + " instead of it).")
+        return "1 1"
 
-            str_nums = input.split()
-            x = int(str_nums[0])
-            y = int(str_nums[1])
+    def manual_test_2_2(self, output):
+        if len(output.split("\n")) > 3:
+            return CheckResult.wrong("We placed a symbol to an occupied cell, but your program didn't warned about it.")
+        return "1 2"
 
-            if i % 2 == 1:
-                full_move_input = f'4 {i}\n' + full_move_input
+    def manual_test_2_3(self, output):
+        tic_tac_toe_field: TicTacToeField = TicTacToeField.parse(output)
+        if tic_tac_toe_field is None:
+            return CheckResult.wrong("The game field is incorrect")
+        if not state2char(tic_tac_toe_field.field[0][0]) == "X":
+            return CheckResult.wrong("The \"O\" symbol overlapped the \"X\" one.")
+        return CheckResult.correct()
 
-            full_game_input = ''
-            for _ in range(9):
-                full_game_input += full_move_input
+    # manual testing of the game #############################################
+    def manual_test_1(self, output):
+        if self.manual_test_turns[self.turn] is None:
+            self.turn += 1
+            temp = self.manual_test_turns[self.turn]
+            return str(temp[0] + 1) + " " + str(temp[1] + 1)
 
-            if i % 4 == 0:
-                initial = "start user easy\n"
-            elif i % 4 == 1:
-                initial = "start easy user\n"
-            elif i % 4 == 2:
-                initial = "start user medium\n"
-            else:
-                initial = "start medium user\n"
+        field: TicTacToeField = TicTacToeField.parse(output)
+        a, b = self.manual_test_turns[self.turn]
+        if self.turn % 2 == 1:
+            mode = "x"
+        else:
+            mode = "o"
 
-            full_game_input = initial + full_game_input + "exit"
+        if field is None:
+            return CheckResult.wrong("The game field is incorrect")
 
-            tests += [TestCase(stdin=full_game_input)]
+        if not str(field.field[a][b]).lower() == "fieldstate." + mode:
+            return CheckResult.wrong("The " + mode.upper() + " was placed to a wrong position." +
+                                     "The " + mode.upper() + " symbol was not found (" +
+                                     state2char(field.field[a][b]) + " instead of it).")
 
-            i += 1
+        self.turn += 1
+        temp = self.manual_test_turns[self.turn]
+        return str(temp[0] + 1) + " " + str(temp[1] + 1)
 
-        tests += [
-            TestCase(stdin="start easy easy\nexit"),
-            TestCase(stdin="start medium medium\nexit"),
-            TestCase(stdin="start medium easy\nexit"),
-            TestCase(stdin="start easy medium\nexit"),
-            TestCase(stdin=
-                     "start user user\n" +
-                     "1 1\n" +
-                     "2 2\n" +
-                     "1 2\n" +
-                     "2 1\n" +
-                     "1 3\n" +
-                     "exit"),
-        ]
+    def manual_test_1_check(self, output: str):
+        if not "wins" in output.lower() or not "x" in output.lower():
+            return CheckResult.wrong("A win message was expected, but the game didn't stop.")
+        return CheckResult.correct()
 
-        return tests
+    ##########################################################################
 
-    def check(self, reply: str, attach: str) -> CheckResult:
+    # input util the finish
+    def test(self, output):
+        index = random.randint(0, len(inputs) - 1)
+        return inputs[index]
+
+    # winnings counters
+    # easy
+    def check_1(self, reply: str, attach):
+        if "x wins" in reply.lower():
+            self.win += 1
+        return CheckResult.correct()
+
+    # check the percentage of winnings
+    def final_check_easy(self, reply, attach):
+        if self.win > 13:
+            self.win = 0
+            self.turn = 0
+            return CheckResult.correct()
+        else:
+            return CheckResult.wrong("The difficulty of your easy AI is too high. " +
+                                     "Make it easier.\n"
+                                     "If you are sure the AI difficulty is fine, try to rerun the test.")
+
+    def final_check_medium(self, reply, attach):
+        print('Total wins:')
+        print(self.win)
+        if self.win > 10:
+            self.win = 0
+            self.turn = 0
+            return CheckResult.correct()
+        else:
+            return CheckResult.wrong("The difficulty of your medium AI is too high. " +
+                                     "Make it easier.\n"
+                                     "If you are sure the AI difficulty is fine, try to rerun the test.")
+
+    # checking if the game works correctly in ai vs ai mode
+    def auto_test_check(self, reply: str, attach):
+        if "wins" not in reply.lower() and "draw" not in reply.lower():
+            return CheckResult.wrong("The game was not finished (your program did not print the result of the game).")
 
         fields = TicTacToeField.parse_all(reply)
 
         if len(fields) == 0:
-            return CheckResult.wrong(
-                "No fields found"
-            )
+            return CheckResult.wrong("No fields found")
 
         for i in range(1, len(fields)):
             curr: TicTacToeField = fields[i - 1]
             next: TicTacToeField = fields[i]
 
-            stayed = curr.equal_to(next)
-            improved = curr.has_next_as(next)
-
-            if not (stayed or improved):
-                return CheckResult.wrong(
-                    "For two fields following each " +
-                    "other one is not a continuation " +
-                    "of the other (they differ more than in two places)."
-                )
+            if not (curr.equal_to(next) or curr.has_next_as(next)):
+                return CheckResult.wrong("For two fields following each " +
+                                         "other one is not a continuation " +
+                                         "of the other (they differ more than in two places).")
 
         return CheckResult.correct()
 
