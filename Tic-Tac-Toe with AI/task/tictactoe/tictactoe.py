8f1af1c1e1ce1ad8dd7b8fa7e6316220998d5922
skyheatfet <70936076+skyheatfet@users.noreply.github.com>
2020-12-23 23:08:39 -0700

    12.23.20.3 Sample Code

--- Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
@@ -1,217 +1,294 @@
-from random import randint
+"""
+A program to play Tic-Tac-Toe
+
+"""
+import random
 
 
-class TicTacToe:
+class Player:
+    def __init__(self, symbol, difficulty="user"):
+        self.symbol = symbol
+        self.difficulty = difficulty
 
-    users = ['user', 'easy', 'medium']
+    def user_move(self):
+        """
+        Completes one move for the game and prints the result
 
-    def __init__(self):
-        self.valid_cells = 'XO_'
-        self.keyboard_input = ''
-        self.level = 'easy'
-        self.player_x = ''
-        self.player_o = ''
-        self.ttt_board = [[f'not.used.{x}:{y}' if x == 0 or y == 0 else ' ' for y in range(4)] for x in range(4)]
+        returns:
+            None
+        """
+        move_coordinate = input("Enter the coordinates:")
+        try:
+            coordinate = list(map(int, move_coordinate.split()))
+            if any(coord not in range(1, 4) for coord in coordinate):
+                print("Coordinates should be from 1 to 3!")
+                self.user_move()
 
-    def init_board(self):
-        self.ttt_board = [[f'not.used.{x}:{y}' if x == 0 or y == 0 else ' ' for y in range(4)] for x in range(4)]
-        return self.ttt_board
+            elif table[coordinate[0] - 1][coordinate[1] - 1] == "X" or \
+                    table[coordinate[0] - 1][coordinate[1] - 1] == "O":
+                print("This cell is occupied! Choose another one!")
+                self.user_move()
 
-    @staticmethod
-    def get_action(prompt):
-        print(prompt, end='')
-        keyboard_input = str.upper(input())
-        return keyboard_input
-
-    def coordinate_validation(self):
-        if (len(self.keyboard_input)) == 9:
-            for character in self.keyboard_input:
-                if character in self.valid_cells:
-                    continue
-                else:
-                    return False
-            return True
-        return False
-
-    def move_validation(self):
-        if self.keyboard_input == 'None':
-            return False
-        if 1 < (len(self.keyboard_input)) < 4:
-            if (len(self.keyboard_input)) == 3 and self.keyboard_input[1] == ' ':
-                self.keyboard_input = self.keyboard_input[0] + self.keyboard_input[2]
-            if self.keyboard_input[0] in '123' and self.keyboard_input[1] in '123':
-                if self.ttt_board[int(self.keyboard_input[0])][int(self.keyboard_input[1])] in 'XO':
-                    return False
-                return True
-        if not str.isdigit(self.keyboard_input):
-            print('You should enter numbers!')
-            return False
-        print('Coordinates should be from 1 to 3!')
-        return False
-
-    def populate_coordinates(self):
-        character_position = 0
-        for y in range(1, 4):  # y counts from top down (bottom up is (3,0 -1))
-            for x in range(1, 4):
-                self.ttt_board[x][y] = self.keyboard_input[character_position]
-                character_position += 1
-
-    def get_coordinates(self):
-        self.keyboard_input = 'None'
-        self.keyboard_input = self.get_action('Enter cells: >')
-        while not self.coordinate_validation():
-            print('This cell is occupied! Choose another one!')
-            self.keyboard_input = self.get_action('Enter cells: >')
-        self.populate_coordinates()
-
-    def display_board(self):
-        print('---------')
-        for y in range(1, 4):  # y counts from top down (bottom up is (3,0 -1))
-            print('| ', end='')
-            for x in range(1, 4):
-                print(f'{self.ttt_board[x][y]} ', end='')
-            print('|')
-        print('---------')
-
-    def get_move(self):
-        self.keyboard_input = 'None'
-        while not self.move_validation():
-            self.keyboard_input = self.get_action('Enter the coordinates: >')
-        self.ttt_board[int(self.keyboard_input[0])][int(self.keyboard_input[1])] = self.who_moves()
-
-    def who_moves(self):
-        x_char = 0
-        o_char = 0
-        for y in range(1, 4):  # y counts from top down (bottom up is (3,0 -1))
-            for x in range(1, 4):
-                if self.ttt_board[x][y] == 'X':
-                    x_char += 1
-                elif self.ttt_board[x][y] == 'O':
-                    o_char += 1
-        if x_char <= o_char:
-            return 'X'
-        else:
-            return 'O'
-
-    def determine_state(self):
-        for x_or_o in 'XO':
-            for x in range(1, 4):
-                if self.ttt_board[x][1] == x_or_o and self.ttt_board[x][2] == x_or_o and self.ttt_board[x][3] == x_or_o:
-                    return f'{x_or_o} wins'
-        for x_or_o in 'XO':
-            for y in range(1, 4):
-                if self.ttt_board[1][y] == x_or_o and self.ttt_board[2][y] == x_or_o and self.ttt_board[3][y] == x_or_o:
-                    return f'{x_or_o} wins'
-        for x_or_o in 'XO':
-            if self.ttt_board[1][1] == x_or_o and self.ttt_board[2][2] == x_or_o and self.ttt_board[3][3] == x_or_o:
-                return f'{x_or_o} wins'
-        for x_or_o in 'XO':
-            if self.ttt_board[3][1] == x_or_o and self.ttt_board[2][2] == x_or_o and self.ttt_board[1][3] == x_or_o:
-                return f'{x_or_o} wins'
-        for x in range(1, 4):
-            for y in range(1, 4):
-                if ' ' in self.ttt_board[x][y]:
-                    return 'Game not finished'
-
-        return 'Draw'
-
-    def medium_strategy(self):
-        for x_or_o in 'XO':
-            for x in range(1, 4):
-                if self.ttt_board[x][1] == x_or_o and self.ttt_board[x][2] == x_or_o and self.ttt_board[x][3] == ' ':
-                    self.ttt_board[x][3] = self.who_moves()
-                    return True
-                elif self.ttt_board[x][1] == ' ' and self.ttt_board[x][2] == x_or_o and self.ttt_board[x][3] == x_or_o:
-                    self.ttt_board[x][1] = self.who_moves()
-                    return True
-        for x_or_o in 'XO':
-            for y in range(1, 4):
-                if self.ttt_board[1][y] == x_or_o and self.ttt_board[2][y] == x_or_o and self.ttt_board[3][y] == ' ':
-                    self.ttt_board[3][y] = self.who_moves()
-                    return True
-                elif self.ttt_board[1][y] == ' ' and self.ttt_board[2][y] == x_or_o and self.ttt_board[3][y] == x_or_o:
-                    self.ttt_board[1][y] = self.who_moves()
-                    return True
-        for x_or_o in 'XO':
-            if self.ttt_board[1][1] == x_or_o and self.ttt_board[2][2] == x_or_o and self.ttt_board[3][3] == ' ':
-                self.ttt_board[3][3] = self.who_moves()
-                return True
-            elif self.ttt_board[1][1] == ' ' and self.ttt_board[2][2] == x_or_o and self.ttt_board[3][3] == x_or_o:
-                self.ttt_board[1][1] = self.who_moves()
-                return True
-        for x_or_o in 'XO':
-            if self.ttt_board[3][1] == x_or_o and self.ttt_board[2][2] == x_or_o and self.ttt_board[1][3] == ' ':
-                self.ttt_board[1][3] = self.who_moves()
-                return True
-            elif self.ttt_board[3][1] == ' ' and self.ttt_board[2][2] == x_or_o and self.ttt_board[1][3] == x_or_o:
-                self.ttt_board[3][1] = self.who_moves()
-                return True
-        return False
-
-    def make_move(self):
-        print(f'Making move level "{self.level}"')
-        x = 0
-        y = 0
-        self.keyboard_input = 'None'
-
-        if self.level == 'medium' and self.medium_strategy():
-            self.ttt_board[x][y] = self.who_moves()
-            return
-        else:
-            while not self.move_validation():
-                x = randint(1, 3)
-                y = randint(1, 3)
-                self.keyboard_input = str(x) + str(y)
-            self.ttt_board[x][y] = self.who_moves()
-
-    def play_game(self):
-        while self.determine_state() == 'Game not finished':
-            self.display_board()
-            if self.who_moves() == 'X':
-                if self.player_x == 'easy':
-                    self.level = 'easy'
-                    self.make_move()
-                elif self.player_x == 'medium':
-                    self.level = 'medium'
-                    self.make_move()
-                elif self.player_x == 'user':
-                    self.get_move()
-                continue
-            if self.who_moves() == 'O':
-                if self.player_o == 'easy':
-                    self.level = 'easy'
-                    self.make_move()
-                elif self.player_o == 'medium':
-                    self.level = 'medium'
-                    self.make_move()
-                elif self.player_o == 'user':
-                    self.get_move()
-
-        self.display_board()
-
-    def menu_loop(self):
-        self.keyboard_input = 'None'
-        while self.keyboard_input[0] != 'exit':
-            self.keyboard_input = self.get_action('Input command: >')
-            self.keyboard_input = str.lower(self.keyboard_input)
-            self.keyboard_input = self.keyboard_input.split()
-            if self.keyboard_input[0] == 'start' and len(self.keyboard_input) == 3:
-                if self.keyboard_input[1] in tic_tac_toe.users and self.keyboard_input[2] in tic_tac_toe.users:
-                    self.player_x = self.keyboard_input[1]
-                    self.player_o = self.keyboard_input[2]
-            elif self.keyboard_input[0] == 'exit':
-                continue
             else:
-                print('Bad parameters')
-                continue
-            self.init_board()
-            self.play_game()
-            print(self.determine_state())
+                table[coordinate[0] - 1][coordinate[1] - 1] = self.symbol
+                print_table()
+                check_win()
 
-    def main(self):
-        self.menu_loop()
+        except ValueError:
+            print("You should enter numbers!")
+            self.user_move()
+
+    def easy_move(self):
+        """
+        Completes one random move for the computer and prints the result
+
+        returns:
+            None
+        """
+        random_coordinate_1 = random.randint(1, 3)
+        random_coordinate_2 = random.randint(1, 3)
+        coordinate = [random_coordinate_1, random_coordinate_2]
+        if table[coordinate[0] - 1][coordinate[1] - 1] != "_":
+            self.easy_move()
+
+        else:
+            table[coordinate[0] - 1][coordinate[1] - 1] = self.symbol
+            print(f"Making move level \"{self.difficulty}\"")
+            print_table()
+            check_win()
+
+    def medium_move(self, opponent):
+        """
+        Completes one move of medium difficulty for the computer and prints the result
+
+        parameter:
+            opponent - the other player
+
+        returns:
+            None
+        """
+        row_count_1 = 0
+        for row in table:
+            if row.count(self.symbol) == 2 and "_" in row:
+                table[row_count_1][row.index("_")] = self.symbol
+                print(f"Making move level \"{self.difficulty}\"")
+                print_table()
+                check_win()
+                return
+            row_count_1 += 1
+
+        for i in range(len(table)):
+            temp_col = [table[0][i], table[1][i], table[2][i]]
+            if temp_col.count(self.symbol) == 2 and "_" in temp_col:
+                table[temp_col.index("_")][i] = self.symbol
+                print(f"Making move level \"{self.difficulty}\"")
+                print_table()
+                check_win()
+                return
+
+        diagonal_1 = [table[0][0], table[1][1], table[2][2]]
+        diagonal_2 = [table[0][2], table[1][1], table[2][0]]
+        if diagonal_1.count(self.symbol) == 2 and "_" in diagonal_1:
+            table[diagonal_1.index("_")][diagonal_1.index("_")] = self.symbol
+            print(f"Making move level \"{self.difficulty}\"")
+            print_table()
+            check_win()
+            return
+
+        elif diagonal_2.count(self.symbol) == 2 and "_" in diagonal_2:
+            table[diagonal_2.index("_")][2 - diagonal_2.index("_")] = self.symbol
+            print(f"Making move level \"{self.difficulty}\"")
+            print_table()
+            check_win()
+            return
+
+        row_count_2 = 0
+        for row in table:
+            if row.count(opponent.symbol) == 2 and "_" in row:
+                table[row_count_2][row.index("_")] = self.symbol
+                print(f"Making move level \"{self.difficulty}\"")
+                print_table()
+                check_win()
+                return
+            row_count_2 += 1
+
+        for i in range(len(table)):
+            temp_col = [table[0][i], table[1][i], table[2][i]]
+            if temp_col.count(opponent.symbol) == 2 and "_" in temp_col:
+                table[temp_col.index("_")][i] = self.symbol
+                print(f"Making move level \"{self.difficulty}\"")
+                print_table()
+                check_win()
+                return
+
+        if diagonal_1.count(opponent.symbol) == 2 and "_" in diagonal_1:
+            table[diagonal_1.index("_")][diagonal_1.index("_")] = self.symbol
+            print(f"Making move level \"{self.difficulty}\"")
+            print_table()
+            check_win()
+            return
+
+        elif diagonal_2.count(opponent.symbol) == 2 and "_" in diagonal_2:
+            table[diagonal_2.index("_")][2 - diagonal_2.index("_")] = self.symbol
+            print(f"Making move level \"{self.difficulty}\"")
+            print_table()
+            check_win()
+            return
+
+        else:
+            self.easy_move()
 
 
-tic_tac_toe = TicTacToe()
-tic_tac_toe.main()
-#  print(tic_tac_toe.ttt_board)
+player_1 = Player("X")
+player_2 = Player("O")
+
+exit_condition = 0
+
+
+def game_menu():
+    """
+    Prompts player for a command to start or exit the game
+
+    returns:
+        None
+    """
+    global exit_condition
+    input_command = input("Input command:").split()
+    if input_command == ["exit"]:
+        exit_condition += 1
+    else:
+        try:
+            if input_command[0] != "start" or \
+                    (input_command[1] != "user" and input_command[1] != "easy" and input_command[1] != "medium") \
+                    or (input_command[2] != "user" and input_command[2] != "easy" and input_command[2] != "medium"):
+                print("Bad parameters!")
+                game_menu()
+            else:
+                player_1.difficulty = input_command[1]
+                player_2.difficulty = input_command[2]
+
+        except IndexError:
+            print("Bad parameters!")
+            game_menu()
+
+
+table = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+
+
+def print_table():
+    """
+    Prints the matrix "table" as a Tic-Tac-Toe board
+
+    returns:
+        None
+    """
+    for i in range(len(table) * 3):
+        print('-', end='')
+
+    print()
+
+    for row in range(len(table)):
+        print('|', end=' ')
+        for col in range(len(table)):
+            if table[row][col] == '_':
+                print(' ', end=' ')
+            else:
+                print(table[row][col], end=' ')
+        print('|')
+
+    for i in range(len(table) * 3):
+        print('-', end='')
+    print()
+
+
+win = 0
+draw = 0
+
+
+def check_win():
+    """
+    Determines the state of the Tic-Tac_Toe board as "win", "draw", or "Game not finished" and prints the result
+
+    returns:
+        None
+    """
+    global win
+    global draw
+    winning_symbol = ""
+    for i in range(len(table)):
+        if table[i][0] == table[i][1] and table[i][1] == table[i][2] and table[i][0] != "_":
+            win += 1
+            if winning_symbol == "":
+                winning_symbol = table[i][0]
+
+        elif table[0][i] == table[1][i] and table[1][i] == table[2][i] and table[0][i] != "_":
+            win += 1
+            if winning_symbol == "":
+                winning_symbol = table[0][i]
+
+    if table[0][0] == table[1][1] and table[1][1] == table[2][2] and table[0][0] != "_":
+        win += 1
+        if winning_symbol == "":
+            winning_symbol = table[0][0]
+
+    elif table[0][2] == table[1][1] and table[1][1] == table[2][0] and table[0][2] != "_":
+        win += 1
+        if winning_symbol == "":
+            winning_symbol = table[0][2]
+
+    if win > 0:
+        print(winning_symbol, "wins")
+
+    elif not any("_" in cell for cell in table):
+        draw += 1
+        print("Draw")
+
+
+def play_game():
+    """
+    Let's players make their moves until the game ends
+
+    returns:
+        None
+    """
+    global win
+    global draw
+    global table
+    print_table()
+    while win == 0 and draw == 0:
+        if player_1.difficulty == "user":
+            player_1.user_move()
+        elif player_1.difficulty == "easy":
+            player_1.easy_move()
+        elif player_1.difficulty == "medium":
+            player_1.medium_move(player_2)
+
+        if win == 0 and draw == 0:
+            if player_2.difficulty == "user":
+                player_2.user_move()
+            elif player_2.difficulty == "easy":
+                player_2.easy_move()
+            elif player_2.difficulty == "medium":
+                player_2.medium_move(player_1)
+    win = 0
+    draw = 0
+    table = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']] ## what
+
+
+def main():
+    """
+    Main Wrapper Function
+
+    returns:
+        None
+    """
+    while exit_condition == 0:
+        game_menu()
+        if exit_condition == 0:
+            play_game()
+        else:
+            exit(main())
+
+
+if __name__ == "__main__":
+    main()
